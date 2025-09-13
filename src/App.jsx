cff42514b26cb577f627da8f6b1ca0e8f8cb0474
omid2007hope <omid85hope@gmail.com>
2025-09-13 16:32:51 +0200

    A bit fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Page/Home";
+import ProductList from "./Page/ProductList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ProductList" element={<ProductList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Page/ProductList.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import "./Home.css";
+import PageSlider from "../components/PageSlider";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <PageSlider />
+    </>
+  );
+}
+
+export default Home;
