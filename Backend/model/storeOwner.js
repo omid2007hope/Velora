cd18fcb8cd528edf11d8053b2f3695e18a45b0bd
omid2007hope <omid85hope@gmail.com>
2026-04-10 20:22:26 +0200

    storeOwner model

---
+++ Backend/model/storeOwner.js
@@ -0,0 +1,42 @@
+// © 2026 Omid Teimory. All rights reserved.
+// Signature: OmidTeimory-2026
+const mongoose = require("mongoose");
+
+const SellerSchema = new mongoose.Schema(
+  {
+    storeOwnerName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      minlength: 2,
+      maxlength: 120,
+    },
+
+    storeOwnerEmailAddress: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+
+    storeOwnerPasswordHash: {
+      type: String,
+      select: false,
+    },
+
+    storeOwnerProvider: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      enum: ["local", "google"],
+      default: "local",
+      index: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+module.exports = mongoose.model("Seller", SellerSchema);
