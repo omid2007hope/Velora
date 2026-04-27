4de676d225374f6c08a83299899281726fbd1771
omid2007hope <omid85hope@gmail.com>
2026-04-27 12:22:20 +0200

    Updart StoreOwnerSlice

--- Frontend/src/app/redux/slice/StoreOwnerSlice.js
+++ Frontend/src/app/redux/slice/StoreOwnerSlice.js
@@ -6,25 +6,25 @@ const data = getStoredStoreOwner();
 const storeOwnerSlice = createSlice({
   name: "storeOwner",
   initialState: {
-    id: data?._id,
+    id: data?._id || null,
     fullName: data?.fullName || "",
     email: data?.email || "",
-    picture: data?.picture || "",
+    picture: data?.picture || null,
     google: data?.google || false,
   },
   reducers: {
     setStoreOwner: (state, action) => {
-      state.id = action.payload._id;
+      state.id = action.payload._id || null;
       state.fullName = action.payload.fullName;
       state.email = action.payload.email;
-      state.picture = action.payload.picture;
-      state.google = action.payload.google;
+      state.picture = action.payload.picture || null;
+      state.google = action.payload.google ?? false;
     },
     clearStoreOwner: (state) => {
       state.id = null;
       state.fullName = "";
       state.email = "";
-      state.picture = "";
+      state.picture = null;
       state.google = false;
     },
   },
