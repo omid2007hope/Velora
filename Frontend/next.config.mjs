d2cb97d2e401767e7088f011b7c99562b571933b
omid2007hope <omid85hope@gmail.com>
2026-04-28 12:16:17 +0200

    Debug

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -58,7 +58,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${apiProxyTarget}/:path*`,
+        destination: `${apiProxyTarget}/api/:path*`,
       },
     ];
   },
