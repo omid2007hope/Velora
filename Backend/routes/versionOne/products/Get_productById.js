677b40a8238e2f0caa81b6a70e5c77ce8bfa9830
omid2007hope <omidhope2007@gmail.com>
2026-07-01 13:38:07 +0200

    Fix: Update product route to use correct endpoint for fetching product by ID

--- Backend/routes/versionOne/products/Get_productById.js
+++ Backend/routes/versionOne/products/Get_productById.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { getProductById } = require("../../../controller/ProductController");
 
-router.get("/seller/products/:id", getProductById);
+router.get("/products/:id", getProductById);
 
 module.exports = router;
